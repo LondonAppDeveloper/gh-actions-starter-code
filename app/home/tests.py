7049462dac3f4a96c9f0cb2d89a1db5afc6f8820
Mark Winterbottom <mark@winterbottom.me>
2021-04-04 20:46:02 +0100

    Added test.

---
+++ app/home/tests.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase, Client
+
+from django.urls import reverse
+
+# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+    """Unit tests for home page."""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_render_page_200(self):
+        """Test the page is rendered with HTTP 200 response."""
+        url = reverse('home:index')
+        res = self.client.get(url)
+
+        self.assertContains(res, 'Sample')
---
+++ app/home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from home import views
+
+
+app_name = 'home'
+
+
+urlpatterns = [
+    path('/', views.index, name='index'),
+]
